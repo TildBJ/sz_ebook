318a3403064dcd4224a69ef9b1c634ac056d475e
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2015-02-12 17:06:58 +0100

    Scheduler Verion ist not supported anymore

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,11 +17,4 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_SzEbook_Command_ConvertCommandController';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_szebook'] = array(
-	'extension'        => $_EXTKEY,
-	'title'            => 'Convert Task for sz_ebook',
-	'description'      => 'Konvertiert PDF\'s zu turnJs'
-);
-
 ?>
